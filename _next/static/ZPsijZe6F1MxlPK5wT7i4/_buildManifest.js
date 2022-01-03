2385ea235ecdea522abbb2ea4024e0c413d50b9e
dung pham <dung.pham@silkwires-dev.com>
2022-01-03 22:28:40 +0700

    ga

---
+++ _next/static/ZPsijZe6F1MxlPK5wT7i4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/970-70c9cb57ca62aa699297.js","static/chunks/pages/index-c4b1bdb262bd130ae7f6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
