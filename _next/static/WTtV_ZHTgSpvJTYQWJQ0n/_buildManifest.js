b2fd2e5f438d2082c25d3d477379927f3ff20e3e
dung pham <dung.pham@silkwires-dev.com>
2022-01-10 10:21:13 +0700

    10-1

--- _next/static/WTtV_ZHTgSpvJTYQWJQ0n/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/83-06c7962de48f5dca8c7f.js","static/chunks/pages/index-d185f27a8dc3bb7d31c6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/vt4rWvzshfcwk7aYKIkaE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/83-06c7962de48f5dca8c7f.js","static/chunks/pages/index-9c2a3001d2103d949b1f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
